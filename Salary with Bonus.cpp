abba0876b33528ae043cadd4ba9dbc0b81a45fa3
sourav singh <40472249+sourav9124@users.noreply.github.com>
2019-11-09 19:36:04 +0530

    Solved Successfully !!!

---
+++ Salary with Bonus.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	string s;
+	cin>>s;
+	
+	double salary,total_sale;
+	cin>>salary;
+	cin>>total_sale;
+	
+	  double profit=(15*total_sale)/100;
+	  
+	  salary=salary+profit;
+	  cout<<fixed<<setprecision(2)<<salary<<endl;
+	  
+	 
+	 
+}
